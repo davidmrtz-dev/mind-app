540cb7fc2e4ab0dce5f23f72579543c3d2030ab2
davidmrtz-dev <davidelimg1@gmail.com>
2023-03-15 14:03:07 -0600

    [FEAT]: Implemented login

--- src/api/core/Auth.ts
+++ src/api/core/Auth.ts
@@ -2,7 +2,7 @@ import * as Http from '../Http';
 import { IUser, Login } from '../../@types';
 
 export const login = async (params: Login): Promise<IUser> => {
-  const result: any = await Http.post('/api/auth/sign_in', params);
+  const result: any = await Http.post('/api/v1/auth/sign_in', params);
   if (result.headers['access-token']) {
     sessionStorage.setItem('authorization:token', result.headers['access-token']);
   }
@@ -16,14 +16,9 @@ export const login = async (params: Login): Promise<IUser> => {
 };
 
 export const logout = async (): Promise<void> => {
-  await Http.destroy('/api/auth/sign_out', {}, {
+  await Http.destroy('/api/v1/auth/sign_out', {}, {
     'access-token': sessionStorage.getItem('authorization:token') || '',
     client: sessionStorage.getItem('authorization:client') || '',
     uid: sessionStorage.getItem('authorization:uid') || '',
   });
 };
-
-export const ping = async (): Promise<any> => {
-  return await Http.get('/api/user/ping');
-};
-
