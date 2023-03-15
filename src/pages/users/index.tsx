9d7ff3047a919e99d0999ad571acac5f482ec599
davidmrtz-dev <davidelimg1@gmail.com>
2023-03-15 14:20:52 -0600

    [WIP]: Implementing users listing, users page

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,5 +1,26 @@
+import { Typography } from "antd";
+import { useAuthContext } from "../../context/AuthContext";
+import { theme } from "../../Theme";
+
 const UsersPage = (): JSX.Element => {
-  return <>hello users`</>
+  const auth = useAuthContext();
+
+  return (
+    <>
+      <Typography style={{
+        ...theme.texts.brandH5
+      }}>
+        Hi, {auth.user?.name}
+      </Typography>
+      <br />
+      {/* <Outcomes
+        fetchData={fetchOutcomes}
+        updateBalance={fetchBalance}
+        category='Recent Outcomes'
+        type='current'
+      /> */}
+    </>
+  );
 };
 
 export default UsersPage;
