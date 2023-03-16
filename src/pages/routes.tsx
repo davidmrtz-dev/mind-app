ccc707a0a04208a46981ecfac5b4a00824f3c194
davidmrtz-dev <davidelimg1@gmail.com>
2023-03-15 20:36:46 -0600

    [FEAT]: Updated components and routes

--- src/pages/routes.tsx
+++ src/pages/routes.tsx
@@ -27,7 +27,6 @@ const Router = (props: RouteProps) => {
       <PrivateRoute exact key='profile' path='/profile' component={Profile} />
     }
     {/* <PrivateRoute exact key='outcomes' path='/outcomes' component={Outcomes} /> */}
-    {/* <PrivateRoute exact key='incomes' path='/incomes' component={Incomes} /> */}
     <Route exact key='login' path='/login' component={Login} />
     <Route exact key='about' path='/about' component={About} />
     {GeneralRoute(props, auth)}
