14291d2aeb32be48c1676fb51a49b1f7158eaf1c
davidmrtz-dev <davidelimg1@gmail.com>
2023-03-30 14:13:49 -0600

    [WIP]: Applying dry to create action @ account

--- src/components/accounts/AccountCreate.tsx
+++ src/components/accounts/AccountCreate.tsx
@@ -36,7 +36,7 @@ export const AccountCreate = ({
       });
       await handleCreate(account);
     } catch (err: any) {
-      const error = err.errors && err.errors.length && err.errors[0];
+      const error = err?.errors?.[0] || err?.error || '';
       Alert({
         icon: 'error',
         text: (error || 'There was an error, please try again later.'),
