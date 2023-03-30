ca38926489c73b78f8839d99f6188cb190c5edcc
davidmrtz-dev <davidelimg1@gmail.com>
2023-03-30 14:17:54 -0600

    [WIP]: Applying dry to create action @ user team

--- src/components/user-teams/UserTeamCreate.tsx
+++ src/components/user-teams/UserTeamCreate.tsx
@@ -39,7 +39,7 @@ export const UserTeamCreate = ({
       });
       await handleCreate(userTeam);
     } catch (err: any) {
-      const error = err.errors && err.errors.length && err.errors[0];
+      const error = err?.errors?.[0] || err?.error || '';
       Alert({
         icon: 'error',
         text: (error || 'There was an error, please try again later.'),
