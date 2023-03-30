512f99e49168b812e760c1842041d9259071ea7e
davidmrtz-dev <davidelimg1@gmail.com>
2023-03-30 14:19:57 -0600

    [ENHANCE]: Enhanced and applied dry to different resources.

--- src/components/user-teams/UserTeamUpdate.tsx
+++ src/components/user-teams/UserTeamUpdate.tsx
@@ -66,7 +66,7 @@ export const UserTeamUpdate = ({
       await deleteUserTeam(userTeam.id);
       handleDelete && handleDelete(userTeam.id);
     } catch (err: any) {
-      const error = err.errors && err.errors.length && err.errors[0];
+      const error = err?.errors?.[0] || err?.error || '';
       Alert({
         icon: 'error',
         text: (error || 'There was an error, please try again later.')
