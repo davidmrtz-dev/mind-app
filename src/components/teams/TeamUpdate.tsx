9206bfdb781b5155b5791e359a985d30af29ad44
davidmrtz-dev <davidelimg1@gmail.com>
2023-03-30 14:15:57 -0600

    [WIP]: Applying dry to update action @ team

--- src/components/teams/TeamUpdate.tsx
+++ src/components/teams/TeamUpdate.tsx
@@ -42,7 +42,7 @@ export const TeamUpdate = ({
       });
       await handleUpdate(team);
     } catch (err: any) {
-      const error = err.errors && err.errors.length && err.errors[0];
+      const error = err?.errors?.[0] || err?.error || '';
       Alert({
         icon: 'error',
         text: (error || 'There was an error, please try again later.')
