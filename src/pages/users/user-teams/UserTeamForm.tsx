44ff1f84fa4d9cc8924ac4b55007767a31b125c2
davidmrtz-dev <davidelimg1@gmail.com>
2023-03-31 17:46:41 -0600

    [WIP]: Added component for user data

--- src/pages/users/user-teams/UserTeamForm.tsx
+++ src/pages/users/user-teams/UserTeamForm.tsx
@@ -1,4 +1,5 @@
 import { DatePicker, Form, InputNumber, Select, Typography } from "antd";
+import styled from "styled-components";
 import { IUser, IUserTeam } from "../../../@types";
 import { theme } from "../../../Theme";
 
@@ -22,34 +23,38 @@ export const UserTeamForm = ({
       onValuesChange={e => setValues({...values, ...e})}
       style={{ width: '100%' }}
     >
-      <Form.Item label={<Typography.Text style={{ ...theme.texts.brandSubFont }}>
+      <Form.Item label={<Typography.Text style={{ ...theme.texts.brandFont }}>
         User
       </Typography.Text>}
-        name='user_id'>
-        <Typography.Text style={{ width: '100%', ...theme.texts.brandSubFont }}>
-          <strong>Name:</strong> {user.name}, <strong>Email:</strong> {user.email}
-        </Typography.Text>
+        name='user'>
+        <UserData {...user} />
       </Form.Item>
-      <Form.Item label={<Typography.Text style={{ ...theme.texts.brandSubFont }}>
+      <Form.Item label={<Typography.Text style={{ ...theme.texts.brandFont }}>
         Select Team
       </Typography.Text>}
-        name='team_id'>
+        name='select_team'>
         <InputNumber
           min={1}
-          style={{ width: '100%', ...theme.texts.brandSubFont }}
+          style={{ width: '100%', ...theme.texts.brandFont }}
         />
       </Form.Item>
-      <Form.Item label="Start Date" name='start_at'>
+      <Form.Item label={<Typography.Text style={{ ...theme.texts.brandFont }}>
+        Start Date
+      </Typography.Text>} name='start_at'>
         <DatePicker
           style={{ width: '100%' }}
         />
       </Form.Item>
-      <Form.Item label="End Date" name='end_at'>
+      <Form.Item label={<Typography.Text style={{ ...theme.texts.brandFont }}>
+        End Date
+      </Typography.Text>} name='end_at'>
         <DatePicker
           style={{ width: '100%' }}
         />
       </Form.Item>
-      <Form.Item label='Status' name='status'>
+      <Form.Item label={<Typography.Text style={{ ...theme.texts.brandFont }}>
+        Status
+      </Typography.Text>} name='status'>
         <Select
           style={{ width: '100%' }}
           options={[
@@ -60,4 +65,56 @@ export const UserTeamForm = ({
       </Form.Item>
     </Form>
   );
+};
+
+const UserDataContainer = styled.div`
+  width: 100%;
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+`;
+
+const UserData = (user: IUser): JSX.Element => {
+  return<UserDataContainer>
+    <Typography.Text
+      style={{
+        backgroundColor: theme.colors.grays.lighter,
+        borderTopLeftRadius: 'inherit',
+        borderTopRightRadius: 'inherit',
+        width: '100%',
+        paddingLeft: 5,
+        ...theme.texts.brandSubFont
+      }}>
+      Name:
+    </Typography.Text>
+    <Typography.Text
+      style={{
+        backgroundColor: theme.colors.whites.lighter,
+        width: '100%',
+        paddingLeft: 5,
+        ...theme.texts.brandSubFont
+      }}>
+      {user.name}
+    </Typography.Text>
+    <Typography.Text
+      style={{
+        backgroundColor: theme.colors.grays.lighter,
+        width: '100%',
+        paddingLeft: 5,
+        ...theme.texts.brandSubFont
+      }}>
+      Email:
+    </Typography.Text>
+    <Typography.Text
+      style={{
+        backgroundColor: theme.colors.whites.lighter,
+        borderBottomLeftRadius: 'inherit',
+        borderBottomRightRadius: 'inherit',
+        width: '100%',
+        paddingLeft: 5,
+        ...theme.texts.brandSubFont
+      }}>
+      {user.email}
+    </Typography.Text>
+  </UserDataContainer>;
 };
