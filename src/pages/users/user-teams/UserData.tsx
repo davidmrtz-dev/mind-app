00ab6e608f7367fd71603bfb611953135c122399
davidmrtz-dev <davidelimg1@gmail.com>
2023-04-04 12:32:16 -0600

    [ENHANCE]: Updated UserData component

--- src/pages/users/user-teams/UserData.tsx
+++ src/pages/users/user-teams/UserData.tsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 import { IUser } from "../../../@types";
-import { Typography } from "antd";
 import { theme } from "../../../Theme";
+import { BrandSubFontText } from "../../../atoms/text";
 
 const UserDataContainer = styled.div`
   width: 100%;
@@ -12,45 +12,29 @@ const UserDataContainer = styled.div`
 
 export const UserData = (user: IUser): JSX.Element => {
   return<UserDataContainer>
-    <Typography.Text
-      style={{
-        backgroundColor: theme.colors.grays.lighter,
-        borderTopLeftRadius: 'inherit',
-        borderTopRightRadius: 'inherit',
-        width: '100%',
-        paddingLeft: 5,
-        ...theme.texts.brandSubFont
-      }}>
-      Name:
-    </Typography.Text>
-    <Typography.Text
-      style={{
-        backgroundColor: theme.colors.whites.lighter,
-        width: '100%',
-        paddingLeft: 5,
-        ...theme.texts.brandSubFont
-      }}>
-      {user.name}
-    </Typography.Text>
-    <Typography.Text
-      style={{
-        backgroundColor: theme.colors.grays.lighter,
-        width: '100%',
-        paddingLeft: 5,
-        ...theme.texts.brandSubFont
-      }}>
-      Email:
-    </Typography.Text>
-    <Typography.Text
-      style={{
-        backgroundColor: theme.colors.whites.lighter,
-        borderBottomLeftRadius: 'inherit',
-        borderBottomRightRadius: 'inherit',
-        width: '100%',
-        paddingLeft: 5,
-        ...theme.texts.brandSubFont
-      }}>
-      {user.email}
-    </Typography.Text>
+    {BrandSubFontText('Name', {
+      backgroundColor: theme.colors.grays.lighter,
+      borderTopLeftRadius: 'inherit',
+      borderTopRightRadius: 'inherit',
+      width: '100%',
+      paddingLeft: 5
+    })}
+    {BrandSubFontText(user.name, {
+      backgroundColor: theme.colors.whites.lighter,
+      width: '100%',
+      paddingLeft: 5
+    })}
+    {BrandSubFontText('Email', {
+      backgroundColor: theme.colors.grays.lighter,
+      width: '100%',
+      paddingLeft: 5
+    })}
+    {BrandSubFontText(user.email, {
+      backgroundColor: theme.colors.whites.lighter,
+      borderBottomLeftRadius: 'inherit',
+      borderBottomRightRadius: 'inherit',
+      width: '100%',
+      paddingLeft: 5
+    })}
   </UserDataContainer>;
 };
